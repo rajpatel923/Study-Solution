9786175858aaa99e19f0fd7ddce779279ba41af1
rajpatel923 <rajpatel123370@gmail.com>
2025-02-12 01:41:27 -0600

    eureka server and config server done

---
+++ server/services/user-profile/src/main/java/com/studysolution/studysync/UserProfileApplication.java
@@ -0,0 +1,13 @@
+package com.studysolution.studysync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserProfileApplication.class, args);
+	}
+
+}
