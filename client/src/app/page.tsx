14405a6564966174274540a873b8eb3917d2c9c0
Tristan-moon <7tlindley@gmail.com>
2025-02-23 23:47:38 -0600

    Update page.tsx

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,7 +2,7 @@ import ComparingSection from "@/components/common/comparingSection";
 import HeroSection from "@/components/common/heroSection";
 import MainFeatures from "@/components/common/mainFeatures";
 import NavBar from "@/components/common/navBar";
-import Advertisement from "@/components/common/advertizement";
+import Advertisement from "@/components/common/advertisement";
 import AllFeatures from "@/components/common/allFeatures";
 import Footer from "@/components/common/footer";
 
