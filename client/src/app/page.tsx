a13097ca1b2bea899f18c5500917cca7320ee59a
rajpatel923 <rajpatel123370@gmail.com>
2025-03-08 12:52:21 -0600

    importing advertisement and allfeatures component

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,9 +2,11 @@ import ComparingSection from "@/components/common/comparingSection";
 import HeroSection from "@/components/common/heroSection";
 import MainFeatures from "@/components/common/mainFeatures";
 import NavBar from "@/components/common/navBar";
-import Advertisement from "@/components/common/advertisement";
-import AllFeatures from "@/components/common/allFeatures";
+
+
 import Footer from "@/components/common/footer";
+import Advertisement from "@/components/common/Bianca/advertisement";
+import AllFeatures from "@/components/common/Bianca/allFeatures";
 
 
 export default function Home() {
